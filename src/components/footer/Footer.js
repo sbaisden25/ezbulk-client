b8ab5cc650a0c7f13f21efb234d1cd694d3cc9bf
sbaisden25 <spbaisden@gmail.com>
2022-06-09 16:57:56 -0400

    newsletter

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './footer.css';
+import 'react-mailchimp-email-signup-form/dist/esm/index.css';
+import { ReactMailchimpEmailSignupForm } from 'react-mailchimp-email-signup-form';
 
 export default class Footer extends Component {
 
@@ -8,7 +10,16 @@ export default class Footer extends Component {
 
         <footer className="footer">
 
+            <ReactMailchimpEmailSignupForm
+              elementId="first-email-signup-form"
+              url="https://Ezbulk.us18.list-manage.com/subscribe/post?u=7318f828a9e97ab10d05acef8&amp;id=a1262e1b0d"
+              title="Get the latest news and updates about Ezbulk"
+              subtitle=""
+            />
+
             <a class="feedback-button" target="_blank" href="https://www.buymeacoffee.com/ezbulkorg">Buy me a coffee ☕</a>
+
+
 
         </footer>
 
