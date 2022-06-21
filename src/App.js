aaae237b7005e25f49c41612a36af6c0b0fe41b1
sbaisden25 <spbaisden@gmail.com>
2022-06-21 15:39:22 -0400

    ga

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-import { useEffect } from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import ProductList from './components/product-list/ProductList';
 import StackList from './components/stack-list/StackList';
 
-import ReactGA from 'react-ga';
-
-  const TRACKING_ID = "UA-231099658-2"; 
-  ReactGA.initialize(TRACKING_ID);
-
 function App() {
-
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname);
-  }, []);
 
   return (
     <BrowserRouter>
